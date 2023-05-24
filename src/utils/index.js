286391d1b8aa64819acc18c55fc6908c412e76f6
gerardoMundo <ger.mundo66@gmail.com>
2023-05-24 14:33:44 -0600

    Index.js and navLinks files added to utils folder

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export * from "./cardsContent";
+export * from "./navLinks";
+export * from "./projects";
+export * from "./tiltOptions";
---
+++ src/utils/navLinks.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    section: "home",
+  },
+  {
+    id: 2,
+    name: "About",
+    section: "about",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    section: "projects",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    section: "contact",
+  },
+];
