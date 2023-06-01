405a35fb75104e6bdf5365c0e4899125371831df
gerardoMundo <ger.mundo66@gmail.com>
2023-05-31 19:53:56 -0600

    Added pictures for projects card component

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,26 +1,26 @@
 export const projects = [
   {
-    title: "Dog Park",
+    title: "Pet Funny!",
     description: "React, MaterialUI, Express, Sql",
     url: "https://parquedog.onrender.com/#/login",
-    image: "../../assets/carrent.png",
+    image: "../../assets/projects/pet_funny.png",
   },
   {
-    title: "PhotoBlogg",
+    title: "Photo Blog",
     description: "React, Redux Toolkit y Firebase",
     url: "photo-blog-7purvrm5r-gerardomundo.vercel.app",
-    image: "../../assets/carrent.png",
+    image: "../../assets/projects/photo_blog.png",
   },
   {
     title: "App calendar",
     description: "React, Node.js, Express, MongoDB",
     url: "https://calendar-backend-zi5m.onrender.com",
-    image: "../../assets/carrent.png",
+    image: "../../assets/projects/calendar_app.png",
   },
   {
     title: "Bibliotecary",
     description: "Anagular, .Net Core, Sql Server",
     url: "pendiente...",
-    image: "../../assets/carrent.png",
+    image: "../../assets/projects/carrent.png",
   },
 ];
