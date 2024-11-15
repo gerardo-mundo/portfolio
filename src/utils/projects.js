ebd0e54ec6aa87e6ce3af1f647335b4187e38b32
Gerardo Mundo <87591007+gerardo-mundo@users.noreply.github.com>
2024-11-15 09:03:48 -0600

    Change order of projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,15 +1,9 @@
 export const projects = [
   {
-    title: "Pet Funny!",
-    description: "React, MaterialUI, Express, Sql",
-    url: "https://parquedog.onrender.com/#/login",
-    image: "projects/pet_funny.png",
-  },
-  {
-    title: "Photo Blog",
-    description: "React, Redux Toolkit y Firebase",
-    url: "photo-blog-7purvrm5r-gerardomundo.vercel.app",
-    image: "projects/photo_blog.png",
+    title: "Library management",
+    description: "Angular, :NET Core, Docker",
+    url: "https://library-app-ge4j.onrender.com",
+    image: "projects/sisgeb.png",
   },
   {
     title: "App calendar",
@@ -18,9 +12,15 @@ export const projects = [
     image: "projects/calendar_app.png",
   },
   {
-    title: "Library management",
-    description: "Angular, :NET Core, Docker",
-    url: "https://library-app-ge4j.onrender.com",
-    image: "projects/sisgeb.png",
+    title: "Photo Blog",
+    description: "React, Redux Toolkit y Firebase",
+    url: "photo-blog-7purvrm5r-gerardomundo.vercel.app",
+    image: "projects/photo_blog.png",
+  },
+  {
+    title: "Pet Funny!",
+    description: "React, MaterialUI, Express, Sql",
+    url: "https://parquedog.onrender.com/#/login",
+    image: "projects/pet_funny.png",
   },
 ];
